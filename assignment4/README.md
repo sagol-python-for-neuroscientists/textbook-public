66c9b7c673147db35834adc725f71548d90e898e
HagaiHargil <hagaihargil@gmail.com>
2020-04-19 21:10:43 +0300

    Updated hw4_q1

--- assignment4/README.md
+++ assignment4/README.md
@@ -17,18 +17,32 @@
     Use the `extent` keyword for the `imshow` function to show image with the right boundaries.
 
     ```python
-    def mandel(n: int, thresh: float=50.,
-               xlims: np.ndarray=np.array([-2, 1]), nx: int=1500,
-               ylims: np.ndarray=np.array([-1.5, 1.5]), ny: int=1500):
-        """
-        Computes the Mandelbrot fractal on some given set of numbers. Creates
-        a binary image with a value of 1 if the point belongs to the set.
-        Parameters:
-        n (int): Number of iterations.
-        thresh (float): Threshold which decides if a number is a part of the set.
-        xlims, ylims (np.ndarray): Limits for the computation of the fractal.
-        nx, ny (int): Number of points between xlims.min() and xlims.max() to
-        calculate the set on.
+    def mandel(
+        n: int,
+        thresh: float = 50.0,
+        xlims: np.ndarray = np.array([-2, 1]),
+        nx: int = 1500,
+        ylims: np.ndarray = np.array([-1.5, 1.5]),
+        ny: int = 1500,
+    ) -> np.ndarray:
+        """Computes the Mandelbrot fractal on some given set of numbers.
+
+        Parameters
+        ----------
+        n : int
+            Number of iterations.
+        thresh : float
+            Threshold which decides if a number is a part of the set.
+        xlims, ylims : np.ndarray
+            Limits for the computation of the fractal.
+        nx, ny : int
+            Number of points between xlims.min() and xlims.max() to calculate the set on.
+
+        Returns
+        -------
+        img : np.ndarray
+            A binary image with a value of 1 if the point belongs to the set.
+            The shape of the resulting image is (nx, ny).
         """
     ```
 
--- assignment4/hw4_q1.py
+++ assignment4/hw4_q1.py
@@ -6,39 +6,51 @@ import matplotlib
 import matplotlib.pyplot as plt
 
 
+def mandel(
+    n: int,
+    thresh: float = 50.0,
+    xlims: np.ndarray = np.array([-2, 1]),
+    nx: int = 1500,
+    ylims: np.ndarray = np.array([-1.5, 1.5]),
+    ny: int = 1500,
+) -> np.ndarray:
+    """Computes the Mandelbrot fractal on some given set of numbers.
 
-def mandel(n: int, thresh: float=50.,
-           xlims: np.ndarray=np.array([-2, 1]), nx: int=1500,
-           ylims: np.ndarray=np.array([-1.5, 1.5]), ny: int=1500):
-    """
-    Computes the Mandelbrot fractal on some given set of numbers. Creates
-    a binary image with a value of 1 if the point belongs to the set.
-    Parameters:
-    n (int): Number of iterations.
-    thresh (float): Threshold which decides if a number is a part of the set.
-    xlims, ylims (np.ndarray): Limits for the computation of the fractal.
-    nx, ny (int): Number of points between xlims.min() and xlims.max() to
-    calculate the set on.
-    """
+    Parameters
+    ----------
+    n : int
+        Number of iterations.
+    thresh : float
+        Threshold which decides if a number is a part of the set.
+    xlims, ylims : np.ndarray
+        Limits for the computation of the fractal.
+    nx, ny : int
+        Number of points between xlims.min() and xlims.max() to calculate the set on.
 
-    # A grid of c-values
+    Returns
+    -------
+    mandelbrot_set : np.ndarray
+        A binary image with a value of 1 if the point belongs to the set.
+        The shape of the resulting image is (nx, ny).
+    """
     x = np.linspace(xlims[0], xlims[1], nx)
     y = np.linspace(ylims[0], ylims[1], ny)
 
+    # Broadcasting rules makes this line create the base image
     c = x[:, np.newaxis] + 1j * y[np.newaxis, :]
 
-    # Mandelbrot iteration
     z = c
     for _ in range(n):
-        z = z**2 + c
+        z = z ** 2 + c
 
     mandelbrot_set = np.abs(z) < thresh
     return mandelbrot_set
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     mandelbrot_set = mandel(50)
     fig, ax = plt.subplots()
-    ax.imshow(mandelbrot_set.T, extent=[-2, 1, -1.5, 1.5], cmap='gray')
+    ax.imshow(mandelbrot_set.T, extent=[-2, 1, -1.5, 1.5], cmap="gray")
+    ax.axis('off')
     plt.show()
-    fig.savefig('mandelbrot.png')
+    fig.savefig("mandelbrot.png")
