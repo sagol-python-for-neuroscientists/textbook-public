2b58e0159d94e08eeb116b12c66dd94c375967cc
Gal Kepler <galkepler@gmail.com>
2024-07-18 11:03:11 +0300

    added linting and formatting to class 8

---
+++ demo.py
@@ -0,0 +1,5 @@
+"""
+This is a demo file to show how to use pylint.
+"""
+
+long_string = "This is a long string that will trigger the line-too-long (E501) lint error"  # Line too long (E501) lint error
