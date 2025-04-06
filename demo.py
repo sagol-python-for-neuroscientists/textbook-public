cb15e417620df50cfa53250551b23791dacf0c45
GalKepler <galkepler@gmail.com>
2025-04-06 11:58:22 +0300

    updated inheritance image

--- demo.py
+++
@@ -1,5 +0,0 @@
-"""
-This is a demo file to show how to use pylint.
-"""
-
-long_string = "This is a long string that will trigger the line-too-long (E501) lint error"  # Line too long (E501) lint error
